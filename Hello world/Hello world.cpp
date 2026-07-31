cd575e359940ad261a83f68a97ca43e32d2dc35b
jenyafishka <ekurbackij4@gmail.com>
2026-08-01 00:15:34 +0500

    Just fixes and boom! Another fixes

---
+++ Hello world/Hello world.cpp
@@ -0,0 +1,13 @@
+﻿// init lib for console out
+#include <iostream>
+// line down for compilator to he know if i write cout to he must std:cout but in code we just write cout or vectorn cin 
+using namespace std;
+
+// do a function with name main compilator knows main executes first
+int main() {
+	// The main code to print hello world
+	cout << "Hello world" << endl;
+	// return is a function for return any number 0-code run succefuly and other
+	return 0;
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Hello world
+# Hello world in cpp
+Hello there! Here you can see code of cpp hello world in folder Console is a Console.cpp sorry idk how it works but here is code hello world in cpp
